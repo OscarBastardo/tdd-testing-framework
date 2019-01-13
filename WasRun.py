105f386aab96a1da077e2dac30217e02259de47b
Oscar Bastardo <osalbr.14@gmail.com>
2019-01-13 23:45:10 +0000

    write testing framework from part two of TDD book

---
+++ WasRun.py
@@ -0,0 +1,15 @@
+from TestCase import TestCase
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        self.wasSetUp = None
+        TestCase.__init__(self, name)
+    def setUp(self):
+        self.log = 'setUp'
+    def testMethod(self):
+        self.log = self.log + ' testMethod'
+    def tearDown(self):
+        self.log = self.log + ' tearDown'
+    def testBrokenMethod(self):
+        raise Exception
